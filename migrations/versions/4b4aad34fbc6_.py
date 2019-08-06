c553191646aa9fad2823b2c98bcc7a6c845f6c58
JOFLIX <joflixooko@outlook.com>
2019-08-06 23:34:15 +0300

    deployment to heroku

---
+++ migrations/versions/4b4aad34fbc6_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4b4aad34fbc6
+Revises: 
+Create Date: 2019-08-06 23:05:23.208556
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4b4aad34fbc6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
